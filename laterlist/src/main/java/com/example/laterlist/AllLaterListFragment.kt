266c9b1639660f952807562a9acc98a97db8f3b7
coderwdd <coderwdd@gmail.com>
2022-11-30 22:58:38 +0800

    category plate add

--- laterlist/src/main/java/com/example/laterlist/AllLaterListFragment.kt
+++ laterlist/src/main/java/com/example/laterlist/AllLaterListFragment.kt
@@ -1,12 +1,15 @@
 package com.example.laterlist
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.LinearLayoutCompat
 import com.example.common.constants.RoutePathConstant
 import com.example.common.custom.BaseFragment
+import com.example.common.log.LaterLog
 import com.example.laterlist.databinding.FragmentAllLaterListBinding
 import com.therouter.router.Route
 
@@ -16,6 +19,8 @@ import com.therouter.router.Route
 )
 class AllLaterListFragment : BaseFragment<FragmentAllLaterListBinding>(FragmentAllLaterListBinding::inflate) {
 
+    @SuppressLint("InflateParams")
     override fun onCreateView() {
+
     }
 }
